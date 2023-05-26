56f289a6bab9d952f29c40742bca62ddab803ea0
diaasaur <diaasaur@gmail.com>
2023-05-26 12:54:16 +0530

    first commit!

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
+  },
+};
