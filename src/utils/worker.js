c58fb91649d653dc7962828aee9627f5082432ed
diaasaur <diaasaur@gmail.com>
2023-05-26 21:17:54 +0530

    web worker message

--- src/utils/worker.js
+++ src/utils/worker.js
@@ -1,5 +1,8 @@
 import { aiMove } from './ai-move';
 
+// using webworker to off load minmax calc from the main thread
+// if not countdown will be blocked
+
 self.addEventListener('message', event => {
   const { board, difficulty } = event.data;
 
